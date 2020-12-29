50e1453656bff94678e3bfdfb8dea149ce1ce5f8
Ruth Slw <rutaslv@hotmail.lt>
2020-12-29 19:25:42 +0000

    make production tweaks

--- teachers_notes/settings.py
+++ teachers_notes/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['RuthSlw.eu.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static'
 
 MEDIA_STATIC = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
