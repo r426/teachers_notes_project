b5f336a50b44d7bc04c98aa5521eafba32ab1f33
Ruth Slw <rutaslv@hotmail.lt>
2021-01-14 17:40:48 +0000

    change domain name

--- teachers_notes/settings.py
+++ teachers_notes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['RuthSlw.eu.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.englishteachersnotes.com']
 
 # Application definition
 
